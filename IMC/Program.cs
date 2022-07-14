215b2973df08ec7e803e47d9dd7f3dd43658429c
Jesus <je-farias@hotmail.com>
2022-07-14 11:18:22 -0300

    Exercício de IMC concluído 14/07/2022

---
+++ IMC/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Digite sua idade: ");
+            string age = ReadLine();
+            WriteLine("Insira seu  peso atual: ");
+            float wheight = (float)Convert.ToDouble(ReadLine());
+            WriteLine("Insira sua altura: ");
+            float higth = (float)Convert.ToDouble(ReadLine());
+
+            float imc = wheight / (higth * higth);
+            string text;
+
+            if (imc < 18.5)
+                text = "seu IMC é: " + Math.Round(imc, 2) + " e você está abaixo do peso.";
+            else if (imc < 25)
+                text = "seu IMC é: " + Math.Round(imc, 2) + " e você está com peso normal.";
+            else if (imc < 30)
+                text = "seu IMC é: " + Math.Round(imc, 2) + " e você está com sobrepeso.";
+            else if (imc < 40)
+                text = "seu IMC é: " + Math.Round(imc, 2) + " e você está com obesidade.";
+            else
+                text = "seu IMC é: " + Math.Round(imc, 2) + " e você está com obesidade grave.";
+
+            WriteLine($"Você tem: {age} anos de idade e {text}");
+
+
+        }
+    }
+}
